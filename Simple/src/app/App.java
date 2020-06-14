f0ccf07ee5522ea6edf2dade2666ef25ac733e69
raviranjan1996 <raviranjanp94@gmail.com>
2020-06-15 02:54:40 +0530

    created

--- Simple/src/app/App.java
+++ Simple/src/app/App.java
@@ -1,12 +1,39 @@
 package app;
 
 public class App {
+
+static void getOccurenceOfAlphabets(String str)
+{
+    int count[] = new int[256];
+
+    for(int i =0 ; i < str.length() ; i++ )
+
+       count[str.charAt(i)]++;
+
+       char ch[] =new char[str.length()]; 
+
+        for(int i=0 ; i<str.length() ; i++)
+        {
+                ch[i] = str.charAt(i);
+                int find = 0;
+            
+               for(int j =0 ; j <=i ; j++)
+               {
+                    if(str.charAt(i) == ch[j])
+                    find++ ;
+               } 
+                   if(find == 1)
+                           System.out.println(" Occurrence of " + 
+                           str.charAt(i) + " is:" + count[str.charAt(i)]); 
+        }
+}
+
+
     public static void main(String[] args) {
 
-        System.out.println("hello world");
-        System.out.println("in the world");
-        System.out.println("hello ravi.....");
-        System.out.println("hello ranjan1234.....");
+      String str = "Raviranjan patel";
+
+      getOccurenceOfAlphabets(str);
         
     }
     
