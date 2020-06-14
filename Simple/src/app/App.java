b768fb9146cd0f56879e2593253b422e06d5b605
raviranjan1996 <raviranjanp94@gmail.com>
2020-06-15 00:18:02 +0530

    created

---
+++ Simple/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+        System.out.println("in the world");
+        
+    }
+    
+}
